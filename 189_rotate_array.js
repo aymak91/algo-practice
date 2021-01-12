8c3c53b3567542a44002f4fa19265c90b8a8fc65
amakyu <amakyu@gmail.com>
2021-01-11 23:58:17 -0800

    rotate arrays faster

--- 189_rotate_array.js
+++ 189_rotate_array.js
@@ -41,3 +41,20 @@ let rotate = function(nums, k) {
     return nums;
     
 };
+
+let rotateFaster = function(nums, k) {
+    
+    k = k % nums.length;
+    
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length -1);
+};
+
+let reverse = function(nums, start, end) {
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]]
+        start++
+        end--
+    }
+}
